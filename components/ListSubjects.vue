dfff4f8cfead74dc0c70559f394b29c8180e5d4d
Maurice <mauricepurnawan@gmail.com>
2020-12-12 14:54:31 +0700

    adding table

--- components/ListSubjects.vue
+++ components/ListSubjects.vue
@@ -1,6 +1,32 @@
 <template>
   <div>
-    
+    <template>
+  <v-simple-table v-if="loaded"> 
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Name
+          </th>
+          <th class="text-left">
+            Calories
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in desserts"
+          :key="item.name"
+        >
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+  </div>
+</template>
   </div>
 </template>
 
@@ -8,6 +34,8 @@
 export default {
   data() {
     return {
+      loaded: false,
+      headers: ["id", "name"],
       subjects: null,
     };
   },
@@ -20,6 +48,7 @@ export default {
         "http://localhost/maurice_scheduler/listSubjects.php"
       );
       this.subjects = subjects;
+      this.loaded = true;
     },
   },
 };
