adb3097b5c1703f055b4e9be46205cfb1c22572c
Maurice <mauricepurnawan@gmail.com>
2020-12-12 14:22:19 +0700

    initial commit

---
+++ components/ListSubjects.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>list subject</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      subjects: null,
+    };
+  },
+  mounted () {
+    const subjects = await this.$axios.$get("http://localhost/maurice_scheduler/listSubjects.php");
+    this.subjects = subjects;
+  },
+  methods: {
+    async fetchSomething() {
+      const subjects = await this.$axios.$get("http://localhost/maurice_scheduler/listSubjects.php");
+      this.subjects = subjects;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/subjects.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <ListSubjects></ListSubjects>
+    </div>
+</template>
+
+<script>
+import ListSubjects from '~/components/ListSubjects.vue'
+    export default {
+        components: {
+            ListSubjects,
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
